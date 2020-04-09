067e972dee437b2baa7b100840428c4ad93d8bac
Brandon Swain <bswai499@students.bju.edu>
2020-04-09 14:44:28 -0400

    Tests for HighScores and PlayerData

--- Tank_Game/src/test/java/model/HighScoresTest.java
+++ Tank_Game/src/test/java/model/HighScoresTest.java
@@ -9,6 +9,18 @@ import org.junit.Test;
 
 public class HighScoresTest {
 
-    
+    @Test
+    public void testHighScores_AddHighScore_Success() {
+        HighScores highScores = new HighScores();
+        highScores.addHighScore("Jeremy", 23000);
+        highScores.addHighScore("Michael", 35000);
 
+        ArrayList<PlayerData> list = highScores.getHighScores();
+
+        assertEquals("Jeremy", list.get(0).getName());
+        assertEquals(23000, list.get(0).getHighScore());
+
+        assertEquals("Michael", list.get(1).getName());
+        assertEquals(35000, list.get(1).getHighScore());
+    }
 }
--- Tank_Game/src/test/java/model/PlayerDataTest.java
+++ Tank_Game/src/test/java/model/PlayerDataTest.java
@@ -9,9 +9,15 @@ public class PlayerDataTest {
 
     @Test
     public void testPlayerData_SetName_Success() {
-        PlayerData player = new PlayerData();
-        
+        PlayerData player = new PlayerData("Shawn", 25000);
+        player.setName("Bob");
+        assertEquals("Bob", player.getName());
     }
-    
 
+    @Test
+    public void testPlayerData_SetHighScore_Success() {
+        PlayerData player = new PlayerData("Shawn", 25000);
+        player.setHighScore(30000);
+        assertEquals(30000, player.getHighScore());
+    }
 }
